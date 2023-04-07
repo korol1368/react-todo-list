8308cd3f4b67a36b8b6bee8509a1f8501372907e
alex <alex@MacBook-Air-Mac.local>
2023-04-07 15:15:16 +0300

    React element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello world');
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+
+const container = document.getElementById('root');
+
+const root = ReactDOMClient.createRoot(container);
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Create awesome app</li>
+    </ul>
+  </div>
+);
+
+root.render(el);
