24846e2b0f206e7af83b081ce0efe788fdf35564
alex <alex@MacBook-Air-Mac.local>
2023-04-07 15:27:16 +0300

    React component

--- src/index.js
+++ src/index.js
@@ -5,15 +5,31 @@ const container = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(container);
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Create awesome app</li>
     </ul>
-  </div>
-);
+  );
+};
 
-root.render(el);
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+root.render(<App />);
